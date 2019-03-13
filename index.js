5002ca09b9448570bacfcb582190bfefce9bccf0
Robin Larsson <robin.larsson@kaustik.com>
2019-03-13 14:36:14 +0100

    Adding socket.io and collecting request from client.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 const BankId = require('bankid')
 
 const bankid = new BankId({ refreshInterval: 2000 })
-const pno = '197001011985'
 
-bankid
-    .authenticateAndCollect('127.0.0.1', pno)
-    .then((res) => console.log(res.completionData))
-    .catch(console.error)
+io.on('connection', function(socket) {
+    console.log('connected')
+    socket.on('authenticateAndCollect', function(pnr) {
+        console.log('pnr: ' + pnr)
+        bankid
+            .authenticateAndCollect('127.0.0.1', pnr)
+            .then((res) => {
+                socket.emit('success')
+                console.log(res.completionData)
+            })
+            .catch((res) => {
+                console.log(res.details)
+            })
+    })
+})
+
+http.listen(5000, function() {
+    console.log('listening on *:5000')
+})
