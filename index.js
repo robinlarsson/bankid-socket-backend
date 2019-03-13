ed1b7eb1a69b4933ee8e7fab2883ba522df3f664
Robin Engström <robin.engstrom@kaustik.com>
2019-03-13 18:01:39 +0100

    index.js: add endpoint
    Signed-off-by: Robin Engström <robin.engstrom@kaustik.com>

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ io.on('connection', function(socket) {
     })
 })
 
+app.get('/secret', function(req, res){
+
+    res.setHeader('Content-Type', 'application/json');
+    if (!req.headers.authorization) {
+        return res.status(403).json({'error': 'No credentials'});
+    }
+
+    token = req.headers.authorization.split(' ')[1];
+
+    nJwt.verify(token,signingKey,function(err, verifiedJwt){
+        if(err) {
+          console.log(err); // Token has expired, has been tampered with, etc
+          return res.status(403).json({'error': 'Invalid credentials'});
+        } else {
+          const pnr = verifiedJwt.body.pnr;
+          console.log(pnr); // Will contain the header and body
+          return res.status(200).json({'pnr': pnr});
+        }
+    });
+
+});
+
 http.listen(5000, function() {
     console.log('listening on *:5000')
 })
