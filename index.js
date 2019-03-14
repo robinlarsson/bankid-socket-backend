6cd053afdd18bc16855ad17e4524e493ac61354a
Robin Larsson <robin.larsson@kaustik.com>
2019-03-14 13:57:08 +0100

    Check if status was collected if page is reloaded.

--- index.js
+++ index.js
@@ -1,40 +1,97 @@
 const app = require('express')()
+const session = require('express-session')
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const BankId = require('bankid')
 const nJwt = require('njwt')
 const cors = require('cors')
-
+const sharedsession = require('express-socket.io-session')
 const signingKey = 'SUPER_SECRET_KEY'
 const bankid = new BankId({ refreshInterval: 2000 })
+const sessionHolder = session({
+    // key: 'token',
+    secret: 'mySecretIsMyPassion',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        expires: 600000,
+    },
+})
 
 app.use(cors())
+app.use(sessionHolder)
+io.use(sharedsession(sessionHolder))
+
+const message = 'BankID'
 
 io.on('connection', function(socket) {
-    console.log('connected')
-    socket.on('authenticateAndCollect', function(pnr) {
+    console.log(`Socket ${socket.id} connected.`)
+    socket.on('authenticate', function(pnr) {
         bankid
-            .authenticateAndCollect('127.0.0.1', pnr)
-            .then((res) => {
-                // Generate a token for this user
-                const claims = {
-                    pnr,
-                }
-                const jwt = nJwt.create(claims, signingKey)
-                socket.emit('success', { token: jwt.compact() })
-                console.log(res.completionData)
+            .authenticate('127.0.0.1', pnr)
+            .then((response) => {
+                socket.emit('orderRef', response.orderRef)
             })
             .catch((response) => {
                 socket.emit('failure', { details: response.details })
             })
     })
+
+    socket.on('checkAlreadyCollectedBankIDStatus', (orderRef) => {
+        console.log('session: ', socket.handshake.session.orderRef)
+        if (socket.handshake.session.orderRef === orderRef) {
+            const token = socket.handshake.session.token
+            socket.emit('success', { token })
+            delete socket.handshake.session.orderRef
+            delete socket.handshake.session.token
+            socket.handshake.session.save()
+            done()
+        }
+    })
+
+    socket.on('collect', function(orderRef) {
+        const timer = setInterval(() => {
+            console.log('orderRef: ', orderRef)
+            const done = () => clearInterval(timer)
+
+            bankid
+                .collect(orderRef)
+                .then((response) => {
+                    if (response.status === 'complete') {
+                        // Generate a token for this user
+                        const claims = {
+                            pnr: response.personalNumber,
+                        }
+                        const jwt = nJwt.create(claims, signingKey)
+                        const token = jwt.compact()
+                        console.log('CONNECTED: ', socket.connected, token)
+                        // if (socket.connected) {
+                        socket.volatile.emit('success', { token })
+                        /*} else {
+                            console.log(token, orderRef)
+                            socket.handshake.session.orderRef = orderRef
+                            socket.handshake.session.token = token
+                            socket.handshake.session.save()
+                        }*/
+                        // console.log(response.completionData)
+                        done()
+                    } else if (response.status === 'failed') {
+                        throw new Error(response.hintCode)
+                    }
+                })
+                .catch((err) => {
+                    console.error(err)
+                    done()
+                })
+        }, 2000)
+    })
 })
 
-app.get('/secret', function(req, res, next) {
-    res.setHeader('Content-Type', 'application/json')
+app.get('/secret', function(req, response, next) {
+    response.setHeader('Content-Type', 'application/json')
 
     if (!req.headers.authorization) {
-        return res.status(403).json({ error: 'No credentials' })
+        return response.status(403).json({ error: 'No credentials' })
     }
 
     token = req.headers.authorization.split(' ')[1]
@@ -42,11 +99,11 @@ app.get('/secret', function(req, res, next) {
     nJwt.verify(token, signingKey, function(err, verifiedJwt) {
         if (err) {
             console.log(err) // Token has expired, has been tampered with, etc
-            return res.status(403).json({ error: 'Invalid credentials' })
+            return response.status(403).json({ error: 'Invalid credentials' })
         } else {
             const pnr = verifiedJwt.body.pnr
             console.log(pnr) // Will contain the header and body
-            return res.status(200).json({ pnr: pnr })
+            return response.status(200).json({ pnr: pnr })
         }
     })
 })
