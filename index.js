9d67424cf8a6a9c2d0f32ad22f0b518f73a93a90
Robin Larsson <robin.larsson@kaustik.com>
2019-03-14 09:25:31 +0100

    Enabling CORS.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const BankId = require('bankid')
 const nJwt = require('njwt')
+const cors = require('cors')
 
 const signingKey = 'SUPER_SECRET_KEY'
 const bankid = new BankId({ refreshInterval: 2000 })
+
+app.use(cors())
 
 io.on('connection', function(socket) {
     console.log('connected')
@@ -27,27 +30,26 @@ io.on('connection', function(socket) {
     })
 })
 
-app.get('/secret', function(req, res){
+app.get('/secret', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json')
 
-    res.setHeader('Content-Type', 'application/json');
     if (!req.headers.authorization) {
-        return res.status(403).json({'error': 'No credentials'});
+        return res.status(403).json({ error: 'No credentials' })
     }
 
-    token = req.headers.authorization.split(' ')[1];
+    token = req.headers.authorization.split(' ')[1]
 
-    nJwt.verify(token,signingKey,function(err, verifiedJwt){
-        if(err) {
-          console.log(err); // Token has expired, has been tampered with, etc
-          return res.status(403).json({'error': 'Invalid credentials'});
+    nJwt.verify(token, signingKey, function(err, verifiedJwt) {
+        if (err) {
+            console.log(err) // Token has expired, has been tampered with, etc
+            return res.status(403).json({ error: 'Invalid credentials' })
         } else {
-          const pnr = verifiedJwt.body.pnr;
-          console.log(pnr); // Will contain the header and body
-          return res.status(200).json({'pnr': pnr});
+            const pnr = verifiedJwt.body.pnr
+            console.log(pnr) // Will contain the header and body
+            return res.status(200).json({ pnr: pnr })
         }
-    });
-
-});
+    })
+})
 
 http.listen(5000, function() {
     console.log('listening on *:5000')
